34ac894f15fbdae030adc22b717a124a8c1361f6
Margarita555 <a_rita@ukr.net>
2022-01-14 17:59:55 +0200

    +

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from '../../redux/auth/auth-selectors';
-import phonebookIcon from '../../images/b.svg';
+import phonebookIcon from '../../images/contacts-book.svg';
 import s from './Navigation.module.css';
 export default function Navigation() {
   const isLoggedIn = useSelector(getIsLoggedIn);
